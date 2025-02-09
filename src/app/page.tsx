141781c0bf8b70b10448d59d6eb1854c8dbd08b9
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2025-02-09 14:52:58 +0530

    feat: setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-pink-500 text-center'>Jai Shree Krishna</h1>
+    </div>
+  )
+}
+
+export default page
